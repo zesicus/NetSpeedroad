8e003666e8ef7e2eafabf8eb3fde5580c3fd62fc
Sunny <zesicus@163.com>
2018-11-07 10:35:27 +0800

    Prepare

--- NetSpeedroad/Others/AppDelegate.swift
+++ NetSpeedroad/Others/AppDelegate.swift
@@ -15,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        Thread.sleep(forTimeInterval: 1.0)
+        
         return true
     }
 
